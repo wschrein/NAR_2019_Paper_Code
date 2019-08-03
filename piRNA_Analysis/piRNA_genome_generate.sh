ca18a6cd5cddcc43ad14cc79f09f2376fead18c6
wschrein <wil.wschreiner@gmail.com>
2019-08-03 14:04:28 -0700

    Add files via upload

---
+++ piRNA_Analysis/piRNA_genome_generate.sh
@@ -0,0 +1,17 @@
+
+
+
+
+#!/bin/bash
+#PBS -q hotel
+#PBS -N jobname
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=1:00:00
+#PBS -o make_wormbase_pirna_genome_t
+#PBS -e make_wormbase_pirna_genome_t
+
+
+
+module load  star/2.5.2b
+
+STAR --runThreadN 8 --runMode genomeGenerate --genomeDir ~/new_pi_testing/pi_genome --genomeFastaFiles ~/new_pi_testing/my_file.fa --genomeSAindexNbases 5 --genomeChrBinNbits 6 
